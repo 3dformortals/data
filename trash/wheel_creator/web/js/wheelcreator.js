d933a23759e639782cdf1b24c511ea2d965d3e5e
user <lenivaya10001@mail.ru>
2018-04-27 15:52:06 +0300

    wheel babylon try

--- trash/wheel_creator/web/js/wheelcreator.js
+++ trash/wheel_creator/web/js/wheelcreator.js
@@ -4,8 +4,45 @@ var geo = new GeometryXD();
 
 // alert(geo.vecXD([1,2,3],[4,5,6]));
 
+var canvas = document.getElementById("renderCanvas");
+
+var engine = new BABYLON.Engine(canvas, true);
+
+var createScene = function () {
+
+	// Create the scene space
+	var scene = new BABYLON.Scene(engine);
+
+	// Add a camera to the scene and attach it to the canvas
+	var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
+	camera.attachControl(canvas, true);
+
+	// Add lights to the scene
+	var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+	var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
 
+	// Add and manipulate meshes in the scene
+	var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+
+	return scene;
+};
+
+/******* End of the create scene function ******/    
+
+var scene = createScene(); //Call the createScene function
+
+engine.runRenderLoop(function () { // Register a render loop to repeatedly render the scene
+scene.render();
+});
+
+
+window.addEventListener("resize", function () { // Watch for browser/canvas resize events
+engine.resize();
+});
+
+
+//old code threejs
 var container;
 var camera, scene, renderer, controls;
 var cp = new THREE.CurvePath();
