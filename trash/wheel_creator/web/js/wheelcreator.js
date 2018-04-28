a46f93724ac1480f168ba1152a28930e615b0375
user <lenivaya10001@mail.ru>
2018-04-28 20:50:05 +0300

    pfff babylon have some bugs, but at least keep the sizes

--- trash/wheel_creator/web/js/wheelcreator.js
+++ trash/wheel_creator/web/js/wheelcreator.js
@@ -5,25 +5,65 @@ var geo = new GeometryXD();
 // alert(geo.vecXD([1,2,3],[4,5,6]));
 
 var canvas = document.getElementById("renderCanvas");
-
+canvas.width = 400;
+canvas.height = 400;
 var engine = new BABYLON.Engine(canvas, true);
-
+var scene;
 var createScene = function () {
 
 	// Create the scene space
-	var scene = new BABYLON.Scene(engine);
+	scene = new BABYLON.Scene(engine);
 
 	// Add a camera to the scene and attach it to the canvas
-	var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
-	camera.attachControl(canvas, true);
+	var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 2, BABYLON.Vector3.Zero(), scene);
+	camera.setPosition(new BABYLON.Vector3(-400, -400, -400));
+    camera.attachControl(canvas, true);
 
 	// Add lights to the scene
 	var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-	var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+	var light3 = new BABYLON.HemisphericLight("light3", new BABYLON.Vector3(0, -1,-1), scene);
+	// var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+	
+	
+	var showAxis = function (size) {
+        var makeTextPlane = function (text, color, size) {
+            var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+            dynamicTexture.hasAlpha = true;
+            dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+            var plane = BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+            plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+            plane.material.backFaceCulling = false;
+            plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+            plane.material.diffuseTexture = dynamicTexture;
+            return plane;
+        };
+        var axisX = BABYLON.Mesh.CreateLines("axisX", [
+            BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+        ], scene);
+        axisX.color = new BABYLON.Color3(1, 0, 0);
+        var xChar = makeTextPlane("X", "red", size / 10);
+        xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+        var axisY = BABYLON.Mesh.CreateLines("axisY", [
+            BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+        ], scene);
+        axisY.color = new BABYLON.Color3(0, 1, 0);
+        var yChar = makeTextPlane("Y", "green", size / 10);
+        yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+        var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
+            BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
+            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
+        ], scene);
+        axisZ.color = new BABYLON.Color3(0, 0, 1);
+        var zChar = makeTextPlane("Z", "blue", size / 10);
+        zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+    };
 
+    showAxis(200);
 
 	// Add and manipulate meshes in the scene
-	var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+	// var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:1}, scene);
 
 	return scene;
 };
@@ -41,23 +81,9 @@ window.addEventListener("resize", function () { // Watch for browser/canvas resi
 engine.resize();
 });
 
-
-//old code threejs
-var container;
-var camera, scene, renderer, controls;
-var cp = new THREE.CurvePath();
-var arc1 =[];
-var arc2 =[];
-var shape;
-var extrudeSettings;
-var mesh;
-var mesh1;
-var mesh2;
-var mesh3;
-init();
-animate();
-
-
+//----------------geometry section
+function vec_maker(vec){ var vec3 = new BABYLON.Vector3(vec[0],vec[1],vec[2]); return vec3; }
+function bez_maker(arc){ var bez = BABYLON.Curve3.CreateCubicBezier(arc[0],arc[1],arc[2],arc[3],200); return bez; }
 function ring_trajectory(dot,vn,va,r){
 	//vn=ox at this moment va = -oz
 	var vb = geo.vec3Drotate(va,vn,90);
@@ -78,130 +104,108 @@ function ring_trajectory(dot,vn,va,r){
 	var ac3 = geo.curve3D_3dots(dot,tad,tbd);
 	var ac4 = geo.curve3D_3dots(dot,tbd,ta);
 	
-	//curves for threejs
+	
+	
+	//curve dots as vectors
 	for (i=0;i<4;i++){
 		a1.push(vec_maker(ac1[i]));
 		a2.push(vec_maker(ac2[i]));
 		a3.push(vec_maker(ac3[i]));
 		a4.push(vec_maker(ac4[i]));
 	};
-	console.log("arc1 curves use threejs vec as dot");
+	console.log("arc1 curves use vec as dot");
 	console.log(a1); // look like done
 	
+	//bezier curves from vector arrays
 	var arc1 = bez_maker(a1);
 	var arc2 = bez_maker(a2);
 	var arc3 = bez_maker(a3);
 	var arc4 = bez_maker(a4);
-	console.log("arc1 bezier curve use threejs")
+	console.log("arc1 bezier curve use babylon")
 	console.log(arc1);
-	cp = new THREE.CurvePath();
-	var arc = [arc1,arc2,arc3,arc4];
 	
-	cp.add(arc[0]);
-	cp.add(arc[1]);
-	cp.add(arc[2]);
-	cp.add(arc[3]);
+	var arc14 = arc1.continue(arc2.continue(arc3.continue(arc4)));
+	var arc14mesh = BABYLON.Mesh.CreateLines("cbezier1", arc14.getPoints(), scene); arc14mesh.color = new BABYLON.Color3(1, 0.6, 0);
+	return arc14.getPoints();
 	// console.log(JSON.stringify(cp).toString());
-	console.log("cp curvepath");
-	console.log(cp);
-	return cp;
-}
-
-function vec_maker(vec){
-	var vec3 = new THREE.Vector3(vec[0],vec[1],vec[2]);
-	return vec3;
-}
-
-function bez_maker(arc){
-	var bez = new THREE.CubicBezierCurve3(arc[0],arc[1],arc[2],arc[3]);
-	return bez;
 }
 
 
 function metal_shape_for_extrusion(h,w,c=[0,0,0]){
 	//bsp - bezier (cubic 2D) spline
-	var r1x = 0; var r1y = 0; var r2x = 0; var r2y = 0; var t2x = 0; var t2y = 0;
-	var bsp=new THREE.Shape();
 	var x=c[0]+2;
 	var y=c[1]+2;
 	var sx = x+w[5]+2; var sy = y+h[8]+2;
-	bsp.moveTo( Math.floor(sx),Math.floor(sy) );//
-	//just triangle shape clockwise
-	bsp.lineTo(Math.floor(sx-w[5]) , Math.floor(sy) );//w5cw to left
-	bsp.lineTo(Math.floor(sx),Math.floor(y+geo.sum_F([h[8],h[7],h[6]])));//to up
-	bsp.lineTo(Math.floor(sx) , Math.floor(sy));//to down
-	return bsp;
+	sx=0; sy=0;
+	console.log("shape before bezX");
+	
+	var bez1 = bez_maker([
+		vec_maker([sx,sy,0]),
+		vec_maker([sx,sy,0]),
+		vec_maker([sx-w[5],sy,0]),
+		vec_maker([sx-w[5],sy,0])
+	]);
+	console.log("after bez1");
+	var bez2 = bez_maker([
+		vec_maker([sx-w[5],sy,0]),
+		vec_maker([sx-w[5],sy,0]),
+		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
+		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0])
+	]);
+	var bez3 = bez_maker([
+		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
+		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
+		vec_maker([sx,sy,0]),
+		vec_maker([sx,sy,0])
+	]);
+	console.log("shape after bezX");
+	var myshape = bez1.continue(bez2.continue(bez3));
+	console.log("myshape");
+	console.log(myshape);
+	console.log("myshape closed");
+	console.log("myshape getPoints = ");
+	console.log(myshape.getPoints());
+	var myshapemesh = BABYLON.Mesh.CreateLines("metalshape", myshape.getPoints(), scene); 
+	myshapemesh.color = new BABYLON.Color3(1, 0.6, 0);
+	return myshape.getPoints();
 }
+//----------------end geometry section
+
 function metal_maker(h, w, hull=false,extrude=100){
 	//metal base of wheel
 	var ox = [1,0,0];
 	var oy = [0,1,0];
 	var c = [0,0,0];
-	var bsp = metal_shape_for_extrusion(h,w,c);//bezier cubic spline for extrusion
-	var dot = [w[5],0,0]; var vn = [1,0,0]; var va = [0,-1,0]; r = Math.floor(h[8]);
+	var dot = [w[5],0,0]; var vn = [1,0,0]; var va = [0,1,0]; r = Math.floor(h[8]);
 	alert(r);
-	var cp = ring_trajectory(dot, vn, va, r);
-	var randomSpline = cp;
-	//
-	extrudeSettings = {
-		steps: 200,//200
-		amount: 12,//not work
-		bevelEnabled: false,
-		extrudePath: randomSpline
-	};
-	var geometry = new THREE.ExtrudeGeometry( bsp, extrudeSettings );
-	var material2 = new THREE.MeshPhysicalMaterial( { color: 0xff00ff, wireframe: false } );
-	mesh3 = new THREE.Mesh( geometry, material2 );
-	scene.add(mesh3);
-	return mesh3;
+	var myPath = ring_trajectory(dot, vn, va, r);
+	var myShape = metal_shape_for_extrusion(h,w,c);//bezier cubic spline for extrusion
+	var extruded = BABYLON.MeshBuilder.ExtrudeShape("ext", {shape: myShape, path: myPath}, scene);
+	console.log("endcode");
+	return extruded;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 function wheel_creator(){
 	d=gui_reader(); //GuiReader.js
 	h=d[0];w=d[1];b=d[2];s=d[3];g=d[4];
 	var angle = 0;
 	var metal = metal_maker(h,w);
-	scene.add(metal);
+	console.log("metal part");
 	console.log(metal);
 }
 
 //------------------------------
-function init() {
-	var info = document.createElement( 'div' );
-	info.style.position = 'absolute';
-	info.style.top = '10px';
-	info.style.width = '100%';
-	info.style.textAlign = 'center';
-	info.style.color = '#fff';
-	info.style.link = '#f80';
-	info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - geometry extrude shapes';
-	document.body.appendChild( info );
-	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
-	// renderer.setSize( window.innerWidth, window.innerHeight );
-	// document.body.appendChild( renderer.domElement );
-	renderer.setSize( 600, 600 );
-	var container = document.getElementById("renderbox");
-	container.appendChild(renderer.domElement);
-	scene = new THREE.Scene();
-	// scene.background = new THREE.Color( 0x222222 );
-	scene.background = new THREE.Color( 0x888888 );
-	// camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera = new THREE.PerspectiveCamera( 45, 1, 1, 1500 );
-	camera.position.set( 0, 0, 500 );
-	// camera.aspect = 1;
-	controls = new THREE.TrackballControls( camera, renderer.domElement );
-	controls.minDistance = 200;
-	controls.maxDistance = 1500;
-	scene.add( new THREE.AmbientLight( 0x888888 ) );
-	var light = new THREE.PointLight( 0xffffff );
-	light.position.copy( camera.position );
-	scene.add( light );
-}
-function animate() {
-	requestAnimationFrame( animate );
-	controls.update();
-	renderer.render( scene, camera );
-}
+	
 
 
