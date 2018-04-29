d3890220b5940dfcb15ce2bbdf3bbdfd7e053b31
user <lenivaya10001@mail.ru>
2018-04-29 19:53:31 +0300

    wheel creator step forward

--- trash/wheel_creator/web/js/wheelcreator.js
+++ trash/wheel_creator/web/js/wheelcreator.js
@@ -83,7 +83,7 @@ engine.resize();
 
 //----------------geometry section
 function vec_maker(vec){ var vec3 = new BABYLON.Vector3(vec[0],vec[1],vec[2]); return vec3; }
-function bez_maker(arc){ var bez = BABYLON.Curve3.CreateCubicBezier(arc[0],arc[1],arc[2],arc[3],200); return bez; }
+function bez_maker(arc){ var bez = BABYLON.Curve3.CreateCubicBezier(arc[0],arc[1],arc[2],arc[3],20); return bez; }
 function ring_trajectory(dot,vn,va,r){
 	//vn=ox at this moment va = -oz
 	var vb = geo.vec3Drotate(va,vn,90);
@@ -131,35 +131,191 @@ function ring_trajectory(dot,vn,va,r){
 }
 
 
+
 function metal_shape_for_extrusion(h,w,c=[0,0,0]){
 	//bsp - bezier (cubic 2D) spline
-	var x=c[0]+2;
-	var y=c[1]+2;
-	var sx = x+w[5]+2; var sy = y+h[8]+2;
-	sx=0; sy=0;
+	var x=c[0];
+	var y=c[1];
+	// var sx = x+w[5]+2; var sy = y+h[8]+2;
+	sx=x+w[5]/2; sy=0;
 	console.log("shape before bezX");
 	
-	var bez1 = bez_maker([
-		vec_maker([sx,sy,0]),
-		vec_maker([sx,sy,0]),
-		vec_maker([sx-w[5],sy,0]),
-		vec_maker([sx-w[5],sy,0])
-	]);
-	console.log("after bez1");
-	var bez2 = bez_maker([
-		vec_maker([sx-w[5],sy,0]),
-		vec_maker([sx-w[5],sy,0]),
-		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
-		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0])
-	]);
-	var bez3 = bez_maker([
-		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
-		vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
-		vec_maker([sx,sy,0]),
-		vec_maker([sx,sy,0])
-	]);
+	var t1; var r1; var r2; var t2;
+	//h87h65
+	t1x = sx; t1y = sy;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1 = t1;
+	r2x = sx; r2y = sy+geo.sum_F([h[7],h[6]]);
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2 = r2;
+	var bez = bez_maker([t1,r1,r2,t2]);
+	
+	//h65h54
+	t1 = t2;
+	r1x = r2x; r1y = r2y+s[4];
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = x+w[3]/2+w[4] ; r2y = r2y+h[5]-s[3] ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y+s[3];
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//h54h43
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x; r2y = r1y+h[4] ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//w4
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t1x; r1y = t1y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x-w[4] ; r2y = r1y;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//h4
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x; r2y = r1y-h[4] ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//w3
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x-w[3] ; r2y = r1y;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	//left mirrored contour
+	//h4m
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x; r2y = r1y+h[4] ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//w4m
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x-w[4] ; r2y = r1y;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//h34h45m
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x; r2y = r1y-h[4] ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//h45h56m
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t2x; r1y = t2y-s[3] ;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = x-w[5]/2 ; r2y = t2y-h[5]+s[4] ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y-s[4] ;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//h56h78m
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t1x; r1y = t1y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = r1x; r2y = r1y-geo.sum_F([h[6],h[7]]) ;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//w5close
+	t1x = t2x; t1y = t2y;
+	t1 = vec_maker([t1x,t1y, 0]);
+	r1x = t1x; r1y = t1y;
+	r1 = vec_maker([r1x,r1y, 0]);
+	r2x = sx; r2y = sy;
+	r2 = vec_maker([r2x,r2y, 0]);
+	t2x = r2x; t2y = r2y;
+	t2 = vec_maker([t2x,t2y, 0]);
+	var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	// //
+	// t1x = ; t1y = ;
+	// t1 = vec_maker([t1x,t1y, 0]);
+	// r1x = ; r1y = ;
+	// r1 = vec_maker([r1x,r1y, 0]);
+	// r2x = ; r2y = ;
+	// r2 = vec_maker([r2x,r2y, 0]);
+	// t2x = ; t2y = ;
+	// t2 = vec_maker([t2x,t2y, 0]);
+	// var bez = bez.continue(bez_maker([t1,r1,r2,t2]));
+	
+	
+	//old triangle code , work done
+	// var bez1 = bez_maker([
+	// 	vec_maker([sx,sy,0]),
+	// 	vec_maker([sx,sy,0]),
+	// 	vec_maker([sx-w[5],sy,0]),
+	// 	vec_maker([sx-w[5],sy,0])
+	// ]);
+	// console.log("after bez1");
+	// var bez2 = bez_maker([
+	// 	vec_maker([sx-w[5],sy,0]),
+	// 	vec_maker([sx-w[5],sy,0]),
+	// 	vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
+	// 	vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0])
+	// ]);
+	// var bez3 = bez_maker([
+	// 	vec_maker([sx,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
+	// 	vec_maker([sx+20,sy+geo.sum_F([h[8],h[7],h[6]]),0]),
+	// 	vec_maker([sx,sy,0]),
+	// 	vec_maker([sx,sy,0])
+	// ]);
 	console.log("shape after bezX");
-	var myshape = bez1.continue(bez2.continue(bez3));
+	// var myshape = bez1.continue(bez2.continue(bez3));
+	var myshape = bez;
 	console.log("myshape");
 	console.log(myshape);
 	console.log("myshape closed");
@@ -180,13 +336,29 @@ function metal_maker(h, w, hull=false,extrude=100){
 	alert(r);
 	var myPath = ring_trajectory(dot, vn, va, r);
 	var myShape = metal_shape_for_extrusion(h,w,c);//bezier cubic spline for extrusion
-	var extruded = BABYLON.MeshBuilder.ExtrudeShape("ext", {shape: myShape, path: myPath}, scene);
+	var extrudeSettings={
+		shape: myShape,
+		path: myPath,
+		// cap: 3, 
+		// sideOrientation:BABYLON.Mesh.DOUBLESIDE,
+		
+	};
+	var customExtrudeSettings={
+		shape: myShape,
+		path: myPath,
+		// ribbonClosePath: true,
+		ribbonCloseArray: true
+		
+	};
+	// var extruded = BABYLON.MeshBuilder.ExtrudeShape("ext", extrudeSettings, scene);
+	var extruded = BABYLON.MeshBuilder.ExtrudeShapeCustom("ext", customExtrudeSettings, scene);
+	// var extruded = BABYLON.MeshBuilder.ExtrudeShape("ext", {shape: myShape, path: myPath}, scene);
 	
 	var mat = new BABYLON.StandardMaterial("mat1", scene);
 	mat.alpha = 1.0;
 	mat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 1.0);
 	mat.backFaceCulling = false;
-	//mat.wireframe = true;
+	// mat.wireframe = true;
 	extruded.material = mat;
 	
 	console.log("endcode");
