721f74150fb6dc5b46156e26e7d83a74295272ea
user <lenivaya10001@mail.ru>
2018-05-13 04:47:01 +0300

    codebox

--- python/codebox/codebox.py
+++ python/codebox/codebox.py
@@ -1,4 +1,5 @@
-import re,sys
+#!/usr/bin/env python3
+import re,sys,string
 
 def mirror_n(t,n):
     """t-list n-mirrornumber"""
@@ -27,12 +28,38 @@ def upanddown(t):
             x[i]=x[i].lower()
     return "".join(x)
 
+def opposite(x):
+    num=string.digits
+    numback=string.digits[::-1]
+    big=string.ascii_uppercase
+    bigback=string.ascii_uppercase[::-1]
+    small=string.ascii_lowercase
+    smallback=string.ascii_lowercase[::-1]
+    # print("before dict generator")
+    numdict={key:value for (key,value) in zip(num,numback)}
+    bigdict={key:value for (key,value) in zip(num,numback)}
+    smalldict={key:value for (key,value) in zip(small,smallback)}
+    # print("afterdict generator")
+    sumdict=numdict.copy()
+    sumdict.update(bigdict)
+    sumdict.update(smalldict)
+    rez=""
+    # print("before loop")
+    for i in range(len(x)):
+        xi = x[i]
+        if (xi in sumdict): rez += sumdict[xi]
+        else:rez += "_"
+    # print("after loop")
+    return rez
+
 x=input("input the text\n\n")
 #clean
 try:
-    x=re.sub(r"[^a-zA-Z0-9]", "", x)
+    # x=re.sub(r"[^a-zA-Z0-9]", "", x)
+    x = opposite(x)
     x=mirror(x)
     x=upanddown(x)
+    
     print("\n\n")
     print(x)
         
@@ -40,3 +67,4 @@ try:
 except:
     print(sys.exc_info())
 input("\n\nenter to close")
+
