369641570a5f839f3549301b9ee79c1ad04b2faa
3dformortals <lenivaya10001@mail.ru>
2020-07-04 04:11:35 +0300

    aset.py fixed. videonote dead

--- python/antishiteater/aset.py
+++ python/antishiteater/aset.py
@@ -2,10 +2,36 @@
 # terminal version
 import webbrowser
 import urllib.request #second test
+import random
+
+def getUserAgent():
+    """return random user agent string from list manually collected before"""
+    alist = [
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Opera/9.80 (Windows NT 6.2; Win64; x64) Presto/2.12 Version/12.16",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0",
+        "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+    ]
+    return random.choice(alist)
 
 def gethtml2(url):
-    with urllib.request.urlopen(url) as response:
+    req = urllib.request.Request(
+    url, 
+    data=None, 
+    headers={
+        'User-Agent': getUserAgent()
+    }
+)
+    with urllib.request.urlopen(req) as response:
         html = response.read().decode()
+    # print(html)
     return html
 
 def readyoutube(url,post=False):
@@ -36,22 +62,24 @@ headtext = """======================================================
 helptext = """прочитать список видео 11...16 , потом открыть в браузере 1...9 -> Enter
 ========================================================================"""
 
+
+# 20200704 new stuff
 hrefs = dict()
 titles = dict()
 want = -1
 while(want != 0):
     if (want in range(11,17)):
         webtext = readyoutube(sources[want])
-        items = webtext.split("dir=\"ltr\" title=\"",11)[1:10] #up to 9 elements
+        items = webtext.split("\"title\":{\"accessibility\":{\"accessibilityData\":{\"label\":\"",11)[1:10] #up to 9 elements
         hrefs.clear()
         titles.clear()
         print("="*len(names[want]))
         print(names[want])
         for i in range(len(items)): #get title , create dict of titles and hrefs
-            title = items[i].split("\"  aria-describedby",1)[0]
+            title = items[i].split("\"simpleText\":\"",1)[1].split("\"},\"publishedTimeText\"",1)[0]
             title = title.replace("&quot;", '\"')[:72]
             if (len(title)==72): title += "..."
-            href = "https://www.youtube.com" + items[i].split("href=\"")[1].split("\"",1)[0]
+            href = "https://www.youtube.com" + items[i].split("\"},\"publishedTimeText\":{\"simpleText\":\"",1)[0].split("\"simpleText\":\"",1)[1]
             titles[i]=title
             hrefs[i]=href
             print(str(i+1) + "  "+titles[i])
@@ -62,3 +90,31 @@ while(want != 0):
         print(headtext)
         print(helptext)
     want = int(input()[:2])
+
+# old stuff not worked, youtube change page style
+# hrefs = dict()
+# titles = dict()
+# want = -1
+# while(want != 0):
+#     if (want in range(11,17)):
+#         webtext = readyoutube(sources[want])
+#         items = webtext.split("dir=\"ltr\" title=\"",11)[1:10] #up to 9 elements
+#         hrefs.clear()
+#         titles.clear()
+#         print("="*len(names[want]))
+#         print(names[want])
+#         for i in range(len(items)): #get title , create dict of titles and hrefs
+#             title = items[i].split("\"  aria-describedby",1)[0]
+#             title = title.replace("&quot;", '\"')[:72]
+#             if (len(title)==72): title += "..."
+#             href = "https://www.youtube.com" + items[i].split("href=\"")[1].split("\"",1)[0]
+#             titles[i]=title
+#             hrefs[i]=href
+#             print(str(i+1) + "  "+titles[i])
+#         print(headtext)
+#     elif (want in range(1,len(hrefs)+1)): #show element in browser
+#         webbrowser.open(hrefs[want-1],2)
+#     else :
+#         print(headtext)
+#         print(helptext)
+#     want = int(input()[:2])
