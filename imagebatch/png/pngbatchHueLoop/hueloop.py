cabca59cf3587517f489c0cc2f9e3eaed624ebad
3dformortals <lenivaya10001@mail.ru>
2020-07-05 11:45:29 +0300

    hueloop alive

---
+++ imagebatch/png/pngbatchHueLoop/hueloop.py
@@ -0,0 +1,135 @@
+import os,sys,datetime
+from PIL import Image
+
+if getattr(sys, 'frozen', False):
+    mydir = os.path.dirname(sys.executable)
+elif __file__:
+    mydir = os.path.dirname(os.path.abspath(__file__))
+
+print("------------mydir------------")
+print(mydir)
+print("------------------------")
+maskdir = mydir+os.sep+"mask"+os.sep
+print("new files will be placed inside ",maskdir, "path")
+print("------------------------")
+
+color = input("new color for pixels which had alpha > 0 (R G B A 255) space separated, default '255 255 255' white with old alpha ")
+
+def pv(x):
+    """parse string to int value between 0...255"""
+    v = int(x) or 255
+    if (v<0) : v=-v
+    if (v>255) : v=255
+    return v
+
+def nextH(h):
+    nh = h + 50
+    if nh>255:nh -= 255
+    return nh
+
+try:
+    color=color.split(" ")
+    print(color)
+    if len(color)==1:#only alpha will be changed
+        _r=None
+        _g=None
+        _b=None
+        _a=pv(color[0])
+    elif len(color)==2:#gray and alpha will be changed
+        _r=pv(color[0])
+        _g=pv(color[0])
+        _b=pv(color[0])
+        _a=pv(color[1])
+    elif len(color)==3:
+        _r=pv(color[0])
+        _g=pv(color[1])
+        _b=pv(color[2])
+        _a=None
+    else:
+        _r = pv(color[0])
+        _g = pv(color[1])
+        _b = pv(color[2])
+        _a = pv(color[3])
+except:
+    print("bad incoming color, will be used white color with old alpha")
+    print(sys.exc_info())
+    _r,_g,_b,_a=255,255,255,None
+
+
+print("new pixel colors will be RGBA(",_r,_g,_b,_a,")")
+try:
+    print(datetime.datetime.now().time())
+    print("------------------ beginning ----------------------")
+    fnames=[]
+    for file in os.listdir(mydir):
+        if file.endswith(".png"):
+            fnames+=[file]
+    print(fnames)
+    
+    if len(fnames) > 0:
+        if not os.path.exists(maskdir):
+            os.makedirs(maskdir)
+    
+    for fname in fnames:
+
+        adres=mydir+os.sep+fname
+        print(adres)
+        im = Image.open(adres).convert('RGBA')
+
+        #hsv
+        A = im.getchannel('A')
+        print([a for a in A.getdata(0)])
+        im.convert('RGB').convert('HSV')
+        H = im.getdata(0)
+
+        newS = im.getdata(1)
+        newV = im.getdata(2)
+
+        newH = []
+        for h in H:
+            newH.append(nextH(h))
+        nim = []
+        for i in range(len(newH)):
+            nim.append((newH[i],newS[i],newV[i]))
+        # print([i for i in H])
+        # print(newH)
+        HSV = Image.new('HSV',im.size)
+        HSV.putdata(nim)
+        R,G,B = HSV.convert('RGB').split()
+        RGBA = Image.merge('RGBA',(R,G,B,A))
+
+
+
+        # im.load()
+        # w,h=im.size
+        # w=range(w)
+        # h=range(h)
+
+        # R=list(im.getdata(0))
+        # G=list(im.getdata(1))
+        # B=list(im.getdata(2))
+        # alp=list(im.getdata(3)) #alpha tuple 0...255 кортеж прозрачности пикселей
+        # newalp=[]
+        # for i in range(len(alp)):
+        #     r=_r if alp[i]>0 and _r is not None else R[i]
+        #     g=_g if alp[i]>0 and _g is not None else G[i]
+        #     b=_b if alp[i]>0 and _b is not None else B[i]
+        #     a=_a if alp[i]>0 and _a is not None else alp[i]
+        #     newalp.append((r,g,b,a))
+
+        print("recount complete")
+        print(datetime.datetime.now().time())
+        print("------------------ V ----------------------")
+        # im.putdata(newalp)
+        print("load image data complete")
+        print(datetime.datetime.now().time())
+        print("------------------ V ----------------------")
+        # im.save("_"+fname)
+        RGBA.save(maskdir+fname)
+        print("save image complete")
+        print(datetime.datetime.now().time())
+        print("------------------ V ----------------------")
+
+except:
+    print(sys.exc_info())
+input("done (готово) . That close press Enter (enter - закрыть)")
